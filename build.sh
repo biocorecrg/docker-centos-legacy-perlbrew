ad0170ee7d359618caa72296d37f31a9e94c79cc
Toni Hermoso Pulido <toniher@cau.cat>
2018-04-02 18:13:39 +0200

    update build and fixing in centos7

---
+++ build.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+set -ueo pipefail
+
+SOURCE=https://github.com/CRG-CNAG/docker-centos-perlbrew
+VARIANTS=(base pyenv pyenv3 pyenv-java gbrowse)
+BRANCHES=(centos7)
+LATEST=centos7
+BASETAG=biocorecrg/centos-perlbrew
+
+TEMPDIR=$HOME/tmp
+WORKDIR=$TEMPDIR/docker-centos-perlbrew
+
+if [ -d "$WORKDIR" ]; then
+	exit 1
+fi
+
+mkdir -p $WORKDIR
+
+
+function dockerBuildPush () {
+
+	cd $1
+	GROUP=""
+	if [ "${1}" != "base" ]; then
+        	GROUP="-$1"
+	fi
+
+	TAG=":$3"
+ 
+	docker build --no-cache -t $2$GROUP$TAG .
+	docker push $2$GROUP$TAG    
+
+	if [ "$3" == $LATEST ]; then
+		docker build -t $2$GROUP .
+		docker push $2$GROUP
+    	fi
+
+}
+
+
+# Iterate branches
+for i in ${BRANCHES[@]}; do
+	
+	cd $WORKDIR
+
+	mkdir ${i}
+
+	CURDIR=$WORKDIR/$i
+	cd ${CURDIR}
+	
+	git clone $SOURCE .
+
+	BRANCH=$i
+	if [ "${BRANCH}" == ${LATEST} ]; then
+		BRANCH=master
+	fi
+
+	git checkout $BRANCH
+
+	for v in ${VARIANTS[@]}; do
+		cd $CURDIR
+		dockerBuildPush $v $BASETAG $i
+	done
+
+			
+done
+
+# Clean everything
+rm -rf $WORKDIR
+
